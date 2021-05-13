7cc174a9424d1b0299973c59e5377408feb9aaf9
gichan <rl0425@naver.com>
2021-05-13 17:27:15 +0900

    question

---
+++ hash02.js
@@ -0,0 +1,9 @@
+let clothes = [["yellowhat", "headgear"],
+ ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
+
+function solution(clothes){
+    let clothSet = []
+    for(let i=0; i<clothes.length; i++){
+
+    }
+}
